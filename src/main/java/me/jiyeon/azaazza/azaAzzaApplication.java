600dcd1bd371de13daf73cdb237306a5f7a6b469
duswl <go305333@gmail.com>
2025-06-08 20:08:49 +0900

    25.06.08 1일차 히히

---
+++ src/main/java/me/jiyeon/azaazza/azaAzzaApplication.java
@@ -0,0 +1,11 @@
+package me.jiyeon.azaazza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class azaAzzaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(azaAzzaApplication.class, args);
+    }
+}
